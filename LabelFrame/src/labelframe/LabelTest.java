89f0ddc1507f59465dabd8f1f8f6786b10312ebd
Programador-L <60987333+Programador-L@users.noreply.github.com>
2020-03-04 19:00:49 -0300

    Add files via upload

---
+++ LabelFrame/src/labelframe/LabelTest.java
@@ -0,0 +1,15 @@
+package labelframe;
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+    
+    public static void main( String[] args ){
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(1230,900);
+        labelFrame.setVisible(true);
+    }
+            
+    
+}
